d066f536ea1d1f4574bb97475e01b5ff9d465805
xieqc <rcxqc2002@163.com>
2019-11-04 14:50:41 +0800

    kea-dhcp4配置文件的解析

--- dhcp/dhcp.go
+++ dhcp/dhcp.go
@@ -29,7 +29,7 @@ const (
 //                }],
 
 type ParseConfig struct{
-	Result int
+	Result json.Number
 	Arguments DHCPConfig
 }
 type DHCPConfig struct{
@@ -37,35 +37,63 @@ type DHCPConfig struct{
 }
 type Dhcp4Config struct{
 	Authoritative bool
-	BootFileName string `json:boot-file-name`
-	//ClientClasses map[string]interface{} `json:client-classes`
-	ControlSocket controlSocket `json:control-socket`
-	subnet4 []Subnet4Config
+	BootFileName string `"json:boot-file-name"`
+	//ClientClasses map[string]interface{} `json:"client-classes"`
+	ControlSocket ControlSocket `json:"control-socket"`
+	OptionData []Subnet4OptionData `json:"option-data"`
+	Subnet4 []Subnet4Config
+
+	T1Percent json.Number `json:"t1-percent"`
+	T2Percent json.Number `json:"t2-percent"`
+	ValidLifetime json.Number `json:"valid-lifetime"`
 }
-type controlSocket struct{
-	SocketName string `json:socket-name`
-	SocketType string `json:socket-type`
+type ControlSocket struct{
+	SocketName string `json:"socket-name"`
+	SocketType string `json:"socket-type"`
 }
 
 type Subnet4Config struct{
 
-	Four4o6Interface string `json:4o6-interface`
-	Four4o6InterfaceId string `json:4o6-interface-id`
-	Four4o6Subnet string `json:4o6-subnet`
-	Authoritative string `json:authoritative`
-	OptionData []Subnet4OptionData `json:option-data`
+	Four4o6Interface string `json:"4o6-interface"`
+	Four4o6InterfaceId string `json:"4o6-interface-id"`
+	Four4o6Subnet string `json:"4o6-subnet"`
+	Authoritative string `json:"authoritative"`
+	CalculateTeeTimes string `"calculate-tee-times"`
+	Id json.Number
+	MatchClientId bool `json:"match-client-id"`
+	NextServer string `json:"next-server"`
+	OptionData []Subnet4OptionData `json:"option-data"`
 	pools []Subnet4Pools
+	RebindTimer json.Number `json:"rebind-timer"`
+	//Relay interface{}
+	RenewTimer json.Number `json:"renew-timer"`
+	ReservationMode string `json:"reservation-mode"`
+	Reservations []Subnet4Reservations
+	Subnet string
+	T1Percent json.Number `json:"t1-percent"`
+	T2Percent json.Number `json:"t2-percent"`
+	ValidLifetime json.Number `json:"valid-lifetime"`
+}
+type Subnet4Subnet struct{
+
+
+
 }
 type Subnet4OptionData struct{
-	AlwaysSend string `json:always-send`
-	Code string `json:code`
-	CsvFormat bool `json:csv-format`
+	AlwaysSend bool
+	Code json.Number
+	CsvFormat bool `json:"csv-format"`
 	Data string
 	Name string
 	Space string
 }
 type Subnet4Pools struct{
-	pool string
+	OptionData []string `json:"option-data"`
+	Pool string
+}
+type Subnet4Reservations struct{
+	BootFileName string `json:"boot-file-name"`
+	OptionData []Subnet4OptionData
 }
 
 
@@ -110,14 +138,15 @@ func CreateSubnet(service string, subnetIp string, subnetMask string ) error {
 	if(err != nil){
 		return err
 	}
-	var configArr interface{}
+	var configArr ParseConfig
 	err2 := json.Unmarshal([]byte(string(configJson[2:len(configJson)-2])), &configArr)
 	if(err2 != nil){
 		fmt.Println("configJson unmarshall failed")
 	}
 
+	fmt.Printf("configArr: $+v", configArr)
+	//subnet4 := configArr["arguments"]["Dhcp4"]
 
-	fmt.Println("service: " + service + ", configJson: " + configJson)
 
 	return nil
 }
