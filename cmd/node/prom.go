e142b96c07531f61ccb0e21004057c63f646aaa6
bjrobertxie <rcxqc2002@163.com>
2019-12-30 22:00:20 +0800

    prometheus测试

---
+++ cmd/node/prom.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+// 初始化 web_reqeust_total， counter类型指标， 表示接收http请求总次数
+var WebRequestTotal = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "web_reqeust_total",
+		Help: "Number of hello requests in total",
+	},
+	// 设置两个标签 请求方法和 路径 对请求总次数在两个
+	[]string{"method", "endpoint"},
+)
+
+// web_request_duration_seconds，Histogram类型指标，bucket代表duration的分布区间
+var WebRequestDuration = prometheus.NewHistogramVec(
+	prometheus.HistogramOpts{
+		Name:    "node_cpu_seconds_total",
+		Help:    "web request duration distribution",
+		Buckets: []float64{0.1, 0.3, 0.5, 0.7, 0.9, 1},
+	},
+	[]string{"method", "endpoint"},
+)
+
+func main() {
+
+	http.Handle("/metrics", promhttp.Handler())
+	logrus.Fatal(http.ListenAndServe(":1235", nil))
+
+}
