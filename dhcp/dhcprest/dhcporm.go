1b5ace2b00f31ed94386b971d3f63c81f3765752
xiequanchao <rcxqc2002@163.com>
2020-03-14 10:23:18 +0800

    change cockroach db dir

--- dhcp/dhcprest/dhcporm.go
+++ dhcp/dhcprest/dhcporm.go
@@ -10,7 +10,7 @@ import (
 
 const Dhcpv4Ver string = "4"
 
-const CRDBAddr = "postgresql://maxroach@localhost:26257/ddi?ssl=true&sslmode=require&sslrootcert=/root/download/cockroach-v19.2.0/certs/ca.crt&sslkey=/root/download/cockroach-v19.2.0/certs/client.maxroach.key&sslcert=/root/download/cockroach-v19.2.0/certs/client.maxroach.crt"
+const CRDBAddr = "postgresql://maxroach@localhost:26257/ddi?ssl=true&sslmode=require&sslrootcert=/root/cockroach-v19.2.0/certs/ca.crt&sslkey=/root/cockroach-v19.2.0/certs/client.maxroach.key&sslcert=/root/cockroach-v19.2.0/certs/client.maxroach.crt"
 
 var PGDBConn *PGDB
 
