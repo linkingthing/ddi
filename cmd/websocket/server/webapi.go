6891eaa85d0a2ab12120092c419990343c83a400
xiequanchao <rcxqc2002@163.com>
2020-03-14 10:14:58 +0800

    change db to ddi

--- cmd/websocket/server/webapi.go
+++ cmd/websocket/server/webapi.go
@@ -394,7 +394,7 @@ func GetPromRange(promType string, host string, start int, end int, step int) (*
 
 	}
 
-	log.Println("GetPromRange(), out: ", out)
+	//log.Println("GetPromRange(), out: ", out)
 	d := json.NewDecoder(bytes.NewReader([]byte(out)))
 	d.UseNumber()
 	err = d.Decode(&rsp)
@@ -413,7 +413,7 @@ func GetPromRange(promType string, host string, start int, end int, step int) (*
 			if err != nil {
 				log.Println("json marshal err: ", err)
 			}
-			//log.Println("string retJson: ", string(retJson))
+			log.Println("string retJson: ", string(retJson))
 			tmp := string(retJson)
 
 			return &tmp, nil
