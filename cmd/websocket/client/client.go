9bfe902f9203c7c08b48d1e320321ee4eb85ca7c
xiequanchao <rcxqc2002@163.com>
2020-04-22 16:25:19 +0800

    add parentIp to keepalive check

--- cmd/websocket/client/client.go
+++ cmd/websocket/client/client.go
@@ -17,8 +17,8 @@ const (
 	StopCharacter = "\r\n\r\n"
 )
 
-func SocketClient(ip string, port int, role string) {
-	addr := strings.Join([]string{ip, strconv.Itoa(port)}, ":")
+func SocketClient(parentIp string, ip string, port int, role string) {
+	addr := strings.Join([]string{parentIp, strconv.Itoa(port)}, ":")
 	conn, err := net.Dial("tcp", addr)
 
 	if err != nil {
@@ -44,7 +44,8 @@ func main() {
 
 	//get promServer from yaml config file
 	yamlConfig := config.GetConfig("/etc/vanguard/vanguard.conf")
-	ip := yamlConfig.Localhost.ParentIP
+	ip := yamlConfig.Localhost.IP
+	parentIp := yamlConfig.Localhost.ParentIP
 	role := yamlConfig.Localhost.Role
 	if yamlConfig.Localhost.IsDHCP {
 		role = "dhcp"
@@ -54,7 +55,7 @@ func main() {
 	port := utils.WebSocket_Port
 
 	for {
-		SocketClient(ip, port, role)
+		SocketClient(parentIp, ip, port, role)
 		time.Sleep(5 * time.Second)
 	}
 
